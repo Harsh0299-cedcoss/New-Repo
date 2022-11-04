01c3837b3ba3979cc6eb7680ff96f78e080daa8e
Shashank Rai <108118506+shanky-ced@users.noreply.github.com>
2022-11-04 14:49:01 +0530

    Update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,10 +1,9 @@
-const columns = [
+  const columnsChild = [
         {
-            title: 'Image',
+            title: 'image',
             dataIndex: 'main_image',
             render: (src) => <>
-                {src.includes('https') ? <img src={src} alt='' width={'60px'} height='64px' /> : <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png'} alt='' width={'60px'} height='64px' />}
-            </>,
+                {src.includes('https') ? <img src={src} alt='' width={'60px'} height='64px' /> : <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/330px-No-Image-Placeholder.svg.png'} alt='' width={'60px'} height='64px' />} </>
         },
         {
             title: 'Title',
@@ -12,44 +11,29 @@ const columns = [
         },
         {
             title: 'Product Details',
-            dataIndex: 'items',
-            render: (items) =>
+            dataIndex: 'price',
+            render: (price, record) =>
                 <>
-                    {items[0]?.price && <Text> Price : ${items[0].price} </Text>}
-                    {items[0]?.sku && <Text> SKU : {items[0].sku} </Text>}
-                    {items[0]?.barcode ? <Text> Barcode : {items[0].barcode} </Text> : <Text> Barcode : N/A </Text>}
-                    {items[0]?.asin ? <Text> ASIN : {items[0].asin} </Text> : <Text> ASIN : N/A </Text>}
-                </>
-        },
-        {
-            title: 'Template',
-            dataIndex: 'profile',
-            render: (profile) =>
-                <>
-                    {profile?.profile_name ? <Text > {profile.profile_name} </Text> : <Text> N/A </Text>}
+                    {price && <Text> Price : ${price} </Text>}
+                    {record.sku && <Text> SKU : {record.sku} </Text>}
+                    {record.barcode ? <Text> Barcode : {record.barcode} </Text> : <Text> Barcode : N/A </Text>}
+                    {record.asin ? <Text> ASIN : {record.asin} </Text> : <Text> ASIN : N/A </Text>}
                 </>
         },
         {
             title: 'Inventory',
-            dataIndex: 'items',
-            render: (items) =>
-                <Text>{items.reduce((sum, val) => sum += val?.quantity || 0, 0)} in Stock for {items.length} variants</Text>
+            dataIndex: 'quantity',
+            render: (quantity) =>
+                <Text>{quantity}</Text>
         },
         {
             title: 'Amazon Status',
-            dataIndex: 'items',
-            render: (items) => <>{items[0]?.status ? <Badge> items[0]?.status </Badge> : <Badge> N/A </Badge>}</>
+            dataIndex: 'status',
+            render: (status) => <>{status ? <Badge> status </Badge> : <Badge> N/A </Badge>}</>
         },
         {
             title: 'Activity',
-            dataIndex: 'items',
-            render: (items) => <>{items[0]?.process_tags ? <Badge> items[0]?.process_tags </Badge> : <Text> -- </Text>}</>
-        },
-        {
-            title: 'Actions',
-            render: () => <Button> <Icon
-                source={MobileVerticalDotsMajor}
-                color="base"
-            /> </Button>
+            dataIndex: 'process_tags',
+            render: (process_tags) => <>{process_tags ? <Badge> process_tags </Badge> : <Text> -- </Text>}</>
         },
     ];
